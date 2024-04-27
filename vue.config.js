7ade9b5a9a8a694184ebc59abad7f3070cc17d41
Death <546115696@qq.com>
2024-04-27 11:28:13 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,20 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
 module.exports = defineConfig({
+  // publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
   transpileDependencies: true,
   lintOnSave: false,
+  configureWebpack: {
+    name: "ffmpeg-desktop",
+    resolve: {
+      alias: {
+        "@": resolve("src"),
+        components: "@/components",
+      },
+    },
+  },
 });
